3b9278c512828f9aba0eb585ca548dade955d2ea
DiosOne <domaone83@proton.me>
2025-05-06 16:28:19 +1000

    moved dinnerFlep and inputTest to new repo

---
+++ dinnerFlip.py
@@ -0,0 +1,17 @@
+import random
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+print("What are your two options for dinner?")
+option1 = input("Option 1: ")
+option2 = input("Option 2: ")
+print("Your choices are " + option1 + " and " + option2)
+print("Flip for " + option1 + " or " + option2 + "?")
+input("Press enter to flip")
+random_number=random.randint(1,1000)
+if is_even(random_number):
+    print("Heads, we have " + option1)
+else:
+    print("Tails, we have " + option2)
---
+++ inputTest.py
@@ -0,0 +1,4 @@
+# print("What are your two options for dinner?")
+# option1 = input("Option 1: ")
+# option2 = input("Option 2: ")
+# print("Your choices are " + option1 + " and " + option2)
